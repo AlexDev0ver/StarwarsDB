a8601194addd3460e1fc1f6971f8121159583df7
Alex Dan <salaris9315@gmail.com>
2020-06-27 11:27:05 +0700

    change server domain to swapi.dev

---
+++ precache-manifest.a85ec5a2644365cb45af3abca97de7ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d522f9016f722e59ced7",
+    "url": "/static/css/main.6b192c8a.chunk.css"
+  },
+  {
+    "revision": "d522f9016f722e59ced7",
+    "url": "/static/js/main.d522f901.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "56acd26827e055d729ef",
+    "url": "/static/js/2.56acd268.chunk.js"
+  },
+  {
+    "revision": "110d678c2de78bba8713595b6d1e91b5",
+    "url": "/index.html"
+  }
+];
